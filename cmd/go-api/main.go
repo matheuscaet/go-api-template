ccfaf17ab73a9edfcffc04b23655563294912487
Matheus Caetano <matheuseprofissional@gmail.com>
2025-11-29 15:38:16 -0300

    feat: first rabbitmq implemntation

--- cmd/go-api/main.go
+++ cmd/go-api/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 
 	"github.com/matheuscaet/go-api-template/api/handlers"
+	consumer "github.com/matheuscaet/go-api-template/consumers"
 	"github.com/matheuscaet/go-api-template/internal/config"
 )
 
 func main() {
 	fmt.Println("Starting Go API")
 	config.LoadEnvVariables()
+	consumer.Start()
 	handlers.StartServer()
 }
